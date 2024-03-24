002e4cac0ebe58b90b146e294f7f5af954af67e9
김지훈 <greenpig4b@gmail.com>
2024-03-24 11:50:29 +0900

    service 만들ì§기전 권한처리

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.blog._core.errors.exception.Exception403;
 import shop.mtcoding.blog.user.User;
 
 import java.util.List;
@@ -22,7 +23,11 @@ public class BoardController {
     @Transactional
     @PostMapping("/board/{id}/update")
     public String update(@PathVariable Integer id,BoardRequest.UpdateDTO reqDTO){
-
+        User user = (User) session.getAttribute("sesionUser");
+        Board board = boardRepository.fintById(id);
+        if (user.getId() != board.getUser().getId() ){
+            throw new Exception403("게시글의 권한이 없습니다");
+        }
         boardRepository.updateById(id,reqDTO);
         return "redirect:/board/"+id;
     }
@@ -36,6 +41,12 @@ public class BoardController {
 
     @PostMapping("/board/{id}/delete")
     public String delete(@PathVariable Integer id){
+        User user = (User)session.getAttribute("sessionUser");
+        Board board = boardRepository.fintById(id);
+
+        if (user.getId() != board.getUser().getId()){
+            throw new Exception403("삭제할 권한이 없습니다.");
+        }
         boardPersistRepository.deleteById(id);
 
         return "redirect:/";
@@ -66,6 +77,15 @@ public class BoardController {
     @GetMapping("/board/{id}")
     public String detail(@PathVariable Integer id, HttpServletRequest request) {
         Board board = boardRepository.fintById(id);
+        User user = (User) session.getAttribute("sessionUser");
+        boolean isOwner = false;
+        if (user != null){
+            if (user.getId() == board.getUser().getId()){
+                isOwner = true;
+            }
+        }
+
+        request.setAttribute("isOwner",isOwner);
         request.setAttribute("board",board);
         return "board/detail";
     }
--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -5,12 +5,21 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import shop.mtcoding.blog._core.errors.exception.Exception400;
+import shop.mtcoding.blog._core.errors.exception.Exception401;
+import shop.mtcoding.blog._core.errors.exception.Exception403;
+import shop.mtcoding.blog.board.Board;
+import shop.mtcoding.blog.board.BoardRepository;
 import shop.mtcoding.blog.board.BoardRequest;
+
+import java.util.zip.DataFormatException;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,15 +29,25 @@ public class UserController {
 
     @PostMapping("/login")
     public String login(UserRequest.LoginDTO reqDTO){
-        User sessionUser =  userRepository.findByUsernameAndPassword(reqDTO.getUsername(),reqDTO.getPassword());
-        session.setAttribute("sessionUser",sessionUser);
+
+        try {
+            User sessionUser = userRepository.findByUsernameAndPassword(reqDTO.getUsername(),reqDTO.getPassword());
+            session.setAttribute("sessionUser",sessionUser);
+        } catch (EmptyResultDataAccessException e) {
+            throw new Exception401("유저네임 혹은 비밀번호가 틀렸습니다");
+        }
 
         return "redirect:/";
     }
 
     @PostMapping("/join")
     public String join(UserRequest.JoinDTO reqDTO){
-        userRepository.save(reqDTO.toEntity());
+
+        try {
+            userRepository.save(reqDTO.toEntity());
+        } catch (DataIntegrityViolationException e) {
+            throw new Exception400("동일한 유저네임이 존재합니다");
+        }
         return "redirect:/";
     }
 
@@ -51,13 +70,14 @@ public class UserController {
 
     @PostMapping("/user/{id}/update")
     public String update(@PathVariable Integer id, UserRequest.UpdateDTO reqDTO){
+
         User user = (User) session.getAttribute("sessionUser");
+
+
         User newSessionUser = userRepository.updateById(user.getId(),reqDTO);
         session.setAttribute("sessionUser",newSessionUser);
-        return "redirect:/";
+        return "redirect:/board/"+id;
     }
-
-
 
     @GetMapping("/logout")
     public String logout() {
